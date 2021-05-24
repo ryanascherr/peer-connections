fdac82f750f6d2420e950459dfc5e7c9e58391b4
ryanascherr <ryanascherr@gmail.com>
2021-05-24 19:40:57 -0400

    Fixed get route for user information.

--- controllers/userRoutes.js
+++ controllers/userRoutes.js
@@ -1,31 +1,25 @@
 const router   = require('express').Router();
-const { User, Issue } = require('../models/User');
+const { User, Issue } = require('../models');
 const express    = require('express');
 const mysql      = require('mysql');
 const withAuth = require('../utils/auth');
 
-router.get('/user/:id',  async (req, res) => {
+router.get('/user/:id', async (req, res) => {
   try {
     const userData = await User.findByPk(req.params.id, {
-      include: [
-      {
-      model: User,
-      attributes: ['name'],
-      },
-      ]
-  });
-
-    const users = userData.map((project) => project.get({ plain: true }));
-
-    res.render('homepage', {
-      users,
-      // Pass the logged in flag to the template
-      logged_in: req.session.logged_in,
+      
     });
+
+    if (!userData) {
+      res.status(404).json({ message: 'No user found with this id!' });
+      return;
+    }
+
+    res.status(200).json(userData);
   } catch (err) {
     res.status(500).json(err);
   }
-});
+})
 
 //Create New User
 router.post('/', async (req, res) => {
@@ -104,13 +98,78 @@ router.get('/', (req, res) => {
   res.render('homepage');
 });
 
-router.get('/profile', (req, res) => {
-  res.render('profile');
-})
+// router.get('/profile', (req, res) => {
+//   res.render('profile');
+// })
 
 router.get('/login', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/profile');
+    return;
+  }
+
   res.render('login');
-})
+});
+
+
+router.get('/profile', withAuth, async (req, res) => {
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Issue }],
+    });
+
+    const user = userData.get({ plain: true });
+
+    res.render('profile', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+// router.get('user/:id', async (req, res) => {
+//   try {
+//     const 
+//   }
+// })
+
+router.post('/login', async (req, res) => {
+  try {
+    const userData = await User.findOne({ where: { email: req.body.email } });
+
+    if (!userData) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    const validPassword = await userData.checkPassword(req.body.password);
+
+    if (!validPassword) {
+      res
+        .status(400)
+        .json({ message: 'Incorrect email or password, please try again' });
+      return;
+    }
+
+    req.session.save(() => {
+      req.session.user_id = userData.id;
+      req.session.logged_in = true;
+      
+      res.json({ user: userData, message: 'You are now logged in!' });
+    });
+
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
+
 
 
 module.exports = router;
