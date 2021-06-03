d2332ba7f9db81e86ee7fd02335f6dcf41645759
ryanascherr <ryanascherr@gmail.com>
2021-06-02 21:09:27 -0400

    Made minor styling changes to front page.

--- public/js/materialize.js
+++ public/js/materialize.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', function() {
   var elems = document.querySelectorAll('.collapsible');
-  var instances = M.Collapsible.init(elems, options);
+  // var instances = M.Collapsible.init(elems, options);
 });
 
 // Or with jQuery
